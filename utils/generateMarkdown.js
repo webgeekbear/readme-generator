c7b0bb319d4a13473ebbbbbe0791de3c93bde25a
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-11-14 17:52:19 -0800

    Correct generation of license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    return `![{license} license](https://img.shields.io/badge/license-${license}-blue.svg)
+    return `![${license} license](https://img.shields.io/badge/license-${license}-blue.svg)
 `;
   } else {
     return "";
